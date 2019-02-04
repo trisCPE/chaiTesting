4f1496a4ac8f0ba5c2ca77aee15d184e4dfdcd77
trisCPE <tristan.gueneau@cpe.fr>
2019-02-04 19:48:33 +0100

    First commit

---
+++ tests/nullTest.js
@@ -0,0 +1,13 @@
+const chai = require("chai");
+let assert = chai.assert;
+var tea = 'tasty chai';
+
+describe('isNullorNot', function () {
+  it('should be null', function() {
+    assert.isNull(err, 'there wa no error');
+  });
+  it('should not be null', function() {
+    assert.isNotNull(tea, 'great, time for tea!');
+  })
+
+})

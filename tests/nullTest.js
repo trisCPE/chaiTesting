94f2bc4d28ec1b9dfb15e9ed3405cec5f7591406
trisCPE <tristan.gueneau@cpe.fr>
2019-02-04 20:00:42 +0100

    nullTest

--- tests/nullTest.js
+++ tests/nullTest.js
@@ -1,9 +1,17 @@
-const chai = require("chai");
-let assert = chai.assert;
-const tea = 'tasty chai';
+var chai = require('chai');
+var assert = chai.assert;
 
-describe('isNullorNot', function () {
-  it('should be null', function() {
-    assert.isNull(null);
+suite('Unit Tests', function(){
+  
+  // Make ALL tests pass
+  // !! Don't scramble the Assertions. We rely on their order to check the results !!
+  suite('Basic Assertions', function() {
+    /** assert.fail() will always fail. Change it into something more useful... **/
+  
+    /** 1 - Use assert.isNull() or assert.isNotNull() to make the tests pass. **/
+    test('#isNull, #isNotNull', function(){
+      assert.isNull(null, 'this is an optional error description - e.g. null is null');
+      assert.isNotNull( 1, '1 is not null');
+    });
   });
-})
+});
